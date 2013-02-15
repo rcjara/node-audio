d63779492cf131fe52cc3d0dcb9bdc03c4b117dd
Raul Jara <raul.c.jara@gmail.com>
2013-02-15 12:54:34 -0500

    Move startup code to main.js

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,4 +3,16 @@ var echo = function(text) {
   $('body').append($('<p>' + text + '</p>'));
 };
 
+require([  'socket.io/socket.io.js'
+         , './javascripts/interact.js'
+         , './javascripts/keyboardSynth.js'
+        ], function(io, interact, keyboard) { //, interact) {
+  console.log('require ready');
+
+  $(document).ready(function() {
+    console.log("Document Ready");
+    interact.connectToServer();
+    keyboard.activate();
+  });
+});
 
