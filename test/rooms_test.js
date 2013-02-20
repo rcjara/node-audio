a6aa4e532eb0a7f088e417cda3a82db8fc4f9ed4
Raul Jara <raul.c.jara@gmail.com>
2013-02-20 15:30:44 -0500

    Add first available method to rooms

--- test/rooms_test.js
+++ test/rooms_test.js
@@ -3,78 +3,125 @@ var expect = require('expect.js')
   , rooms = require('../lib/rooms.js')
   ;
 
-describe('Implementation Details', function() {
-  describe('.nameForID',function() {
-    it('room names from integers',function(){
-      expect(rooms.nameForID(0))
-            .to.equal('yellow-elephant-hoarders');
+describe('Rooms', function() {
+  beforeEach(function() {
+    rooms.reset();
+  });
+
+  describe('Implementation Details', function() {
+    describe('.nameForID',function() {
+      it('room names from integers',function(){
+        expect(rooms.nameForID(0))
+              .to.equal('yellow-elephant-hoarders');
+      });
+
+      it('produces unique results for different keys', function() {
+        var results = [];
+        for (var i = 0; i < 6; i++) {
+          var j = i + 20; //look at an interesting range that causes all 3 names to rotate over
+          var result = rooms.nameForID(j);
+
+          expect(results.length) // make sure we are storing the results
+                .to.equal(i);
+
+          expect(_.indexOf(results)) // make sure no result is duplicated
+                .to.equal(-1);
+
+          results.push(result);
+        }
+      });
     });
 
-    it('produces unique results for different keys', function() {
-      var results = [];
-      for (var i = 0; i < 6; i++) {
-        var j = i + 20; //look at an interesting range that causes all 3 names to rotate over
-        var result = rooms.nameForID(j);
+    describe('.convertFromName', function() {
+      it('converts valid names to integers', function() {
+        expect(rooms.convertFromName('yellow-elephant-hoarders'))
+              .to.equal(0);
+      });
 
-        expect(results.length) // make sure we are storing the results
-              .to.equal(i);
+      it('can get back the integer used to generate any valid name', function() {
+        for(var i = 0; i < 26; i++) {
+          var name = rooms.nameForID(i);
+          var num  = rooms.convertFromName(name);
 
-        expect(_.indexOf(results)) // make sure no result is duplicated
-              .to.equal(-1);
-
-        results.push(result);
-      }
+          expect(num)
+                .to.equal(i);
+        }
+      });
     });
   });
 
-  describe('.convertFromName', function() {
-    it('converts valid names to integers', function() {
-      expect(rooms.convertFromName('yellow-elephant-hoarders'))
-            .to.equal(0);
+  describe('Actual interface', function() {
+    describe('.generateRoom', function() {
+      it('generates unique room names with each call', function() {
+        var results = [];
+        for (var i = 0; i < 4; i++) {
+          var result = rooms.generateRoom();
+
+          expect(results.length) // make sure we are storing the results
+                .to.equal(i);
+
+          expect(_.indexOf(results.name)) // make sure no result is duplicated
+                .to.equal(-1);
+
+          results.push(result.name);
+        }
+      });
+
+      it('generates rooms with default capacity', function() {
+        var room = rooms.generateRoom();
+        expect(room)
+              .to.have.property('capacity', 4);
+      });
+
+      it('generates unique room objects', function() {
+        var room1 = rooms.generateRoom()
+          , room2 = rooms.generateRoom()
+          ;
+
+        room1.capacity = room1.capacity + 1;
+
+        expect(room1.capacity)
+              .to.not.equal(room2.capacity);
+      });
     });
 
-    it('can get back the integer used to generate any valid name', function() {
-      for(var i = 0; i < 26; i++) {
-        var name = rooms.nameForID(i);
-        var num  = rooms.convertFromName(name);
+    describe('.firstAvailable', function() {
+      it('returns null if no rooms exist', function() {
+        expect(rooms.firstAvailable())
+              .to.equal(null);
+      });
 
-        expect(num)
-              .to.equal(i);
-      }
+      describe('with three rooms', function() {
+        var room1, room2, room3;
+
+        beforeEach(function() {
+          room1 = rooms.generateRoom();
+          room2 = rooms.generateRoom();
+          room3 = rooms.generateRoom();
+        });
+
+        it('finds the available room', function() {
+          room1.occupancy = function() { return 1000; }
+          room2.occupancy = function() { return 1000; }
+          room3.occupancy = function() { return 2; }
+
+          var foundRoom = rooms.firstAvailable();
+
+          expect(foundRoom.name)
+                .to.equal(room3.name);
+        });
+
+        it('returns null if no rooms are below capacity', function() {
+          room1.occupancy = function() { return 1000; }
+          room2.occupancy = function() { return 1000; }
+          room3.occupancy = function() { return 2; }
+
+          var foundRoom = rooms.firstAvailable();
+
+          expect(foundRoom.name)
+                .to.equal(room3.name);
+        });
+      });
     });
   });
 });
-
-describe('Actual interface', function() {
-  describe('.generateRoom', function() {
-    it('generates unique room names with each call', function() {
-      var results = [];
-      // 26 chosen so that it ends up incrementing the last name
-      for (var i = 0; i < 26; i++) {
-        var result = rooms.generateRoom();
-
-        expect(results.length) // make sure we are storing the results
-              .to.equal(i);
-
-        expect(_.indexOf(results)) // make sure no result is duplicated
-              .to.equal(-1);
-
-        results.push(result);
-      }
-    });
-  });
-
-  describe('a generated room', function() {
-    var roomName;
-
-    beforeEach( function() {
-      roomName = rooms.generateRoom();
-    });
-
-    it('has the default capacity', function() {
-      expect(rooms.room(roomName))
-            .to.have.property('capacity', 4);
-    });
-  });
-});
-
