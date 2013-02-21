9c03c8ed48a6b104cd90f65b92a641ccb748ab40
Raul Jara <raul.c.jara@gmail.com>
2013-02-21 17:04:41 -0500

    Get rooms to use the new naming scheme object

--- test/rooms_test.js
+++ test/rooms_test.js
@@ -1,53 +1,14 @@
 var expect = require('expect.js')
   , _ = require('underscore')
   , rooms = require('../lib/rooms.js')
+  , namingScheme = require('../lib/naming-scheme.js')
   ;
+
+rooms.setNamingScheme(namingScheme);
 
 describe('Rooms', function() {
   beforeEach(function() {
     rooms.reset();
-  });
-
-  describe('Implementation Details', function() {
-    describe('.nameForID',function() {
-      it('room names from integers',function(){
-        expect(rooms.nameForID(0))
-              .to.equal('yellow-elephant-hoarders');
-      });
-
-      it('produces unique results for different keys', function() {
-        var results = [];
-        for (var i = 0; i < 6; i++) {
-          var j = i + 20; //look at an interesting range that causes all 3 names to rotate over
-          var result = rooms.nameForID(j);
-
-          expect(results.length) // make sure we are storing the results
-                .to.equal(i);
-
-          expect(_.indexOf(results)) // make sure no result is duplicated
-                .to.equal(-1);
-
-          results.push(result);
-        }
-      });
-    });
-
-    describe('.convertFromName', function() {
-      it('converts valid names to integers', function() {
-        expect(rooms.convertFromName('yellow-elephant-hoarders'))
-              .to.equal(0);
-      });
-
-      it('can get back the integer used to generate any valid name', function() {
-        for(var i = 0; i < 26; i++) {
-          var name = rooms.nameForID(i);
-          var num  = rooms.convertFromName(name);
-
-          expect(num)
-                .to.equal(i);
-        }
-      });
-    });
   });
 
   describe('Actual interface', function() {
